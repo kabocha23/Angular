b5d0b752ecabf6524248c0dc1d4076d833f4facb
kabocha23 <jekobuchi@gmail.com>
2017-10-13 10:59:29 -0700

    TypesAndClasses

---
+++ TypesAndClasses.ts
@@ -0,0 +1,29 @@
+var myNum: number = 5;
+var myString: string = "Hello Universe";
+var myArr: Array<number> = [1,2,3,4];
+var myObj: object = { name:'Bill'};
+var anythingVariable: any = "Hey";
+var anythingVariable: any = 25; 
+var arrayOne = [true, false, true, true]; 
+var arrayTwo = [1, 'abc', true, 2];
+myObj = { x: 5, y: 10 };
+// object constructor
+var MyNode: any = (function () {
+    function MyNode(val) {
+        this.val = 0;
+        this.val = val;
+    }
+    MyNode.prototype.doSomething = function () {
+        this._priv = 10;
+    };
+    return MyNode;
+}());
+var myNodeInstance: any = new MyNode(1);
+console.log(myNodeInstance.val);
+function myFunction() {
+    console.log("Hello World");
+    return;
+}
+function sendingErrors() {
+	throw new Error('Error message');
+}
